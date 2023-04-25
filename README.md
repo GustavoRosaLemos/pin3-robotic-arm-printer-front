01c019787a9352fb050de4bf9a0bc0ac0e627238
Gustavo Lemos <dev.gustavolemos@outlook.com>
2023-04-25 19:13:14 -0300

    Basic setup.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# React V18 Boilerplate
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) TS template.
+# Robotic Arm Printer
 
 ## Available Scripts
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import HealthPage from '../pages/health';
 import store from '../store';
 
-// export const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <div>Hello world!</div>,
-//   },
-//   {
-//     path: '/health',
-//     element: <HealthPage />,
-//   },
-// ]);
-
 function RouterSwitch() {
   return (
     <Provider store={store}>
